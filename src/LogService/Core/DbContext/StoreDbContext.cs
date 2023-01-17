f74f3cc894880bf99adc70ce08e497ccea54d3c1
Mohamed A. Laghrour <35415121+cppJoker@users.noreply.github.com>
2023-01-17 00:11:22 -0500

    Update docker deployment

---
+++ src/LogService/Core/DbContext/StoreDbContext.cs
@@ -0,0 +1,29 @@
+﻿using CommonLibrary.AspNetCore.Core;
+using CommonLibrary.Core;
+using LogService.Logging.Models;
+using Microsoft.EntityFrameworkCore;
+using LogHandle = LogService.Logging.Models.LogHandle;
+
+namespace LogService.Core;
+
+public class StoreDbContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    public StoreDbContext(DbContextOptions<StoreDbContext> opt, IConfiguration configuration) : base(opt)
+    {
+            _configuration = configuration;
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        DatabaseSettings serviceSettings = _configuration.GetSection(nameof(DatabaseSettings)).Get<DatabaseSettings>() ?? throw new InvalidOperationException("ServiceSettings is null");
+        optionsBuilder.UseNpgsql(serviceSettings.StorePostgresConnectionString);
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.BuildCommonLibrary();
+    }
+    public DbSet<LogHandle> LogHandles { get; set; }
+    public DbSet<LogMessage> LogMessages { get; set; }
+}
