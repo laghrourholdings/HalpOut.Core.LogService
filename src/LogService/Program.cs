9d22d0cf7463fee8c75ce4976ad1fb77cc2f7175
Mohamed A. Laghrour <35415121+cppJoker@users.noreply.github.com>
2023-01-25 14:22:02 -0500

    Simplified use of CommonLibrary, added invalidate user and sign out.

--- src/LogService/Program.cs
+++ src/LogService/Program.cs
@@ -15,8 +15,7 @@ var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var logger = new LoggerConfiguration().WriteTo.Console();
 builder.Services.AddDbContext<StoreDbContext>();
 builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection(nameof(DatabaseSettings)));
-builder.Services.AddCommonLibrary(builder.Configuration, builder.Logging, logger , MyAllowSpecificOrigins);
-builder.Services.AddCommonLibrarySecuroman();
+builder.Services.AddCommonLibrary(builder.Configuration, builder.Logging, logger , MyAllowSpecificOrigins, true, false);
 //builder.Services.AddScoped<IRepository<LogMessage>, LogMessageRepository>();
 builder.Services.AddScoped<ILoggingService, LoggingService>();
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
@@ -26,7 +25,6 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 app.UseCommonLibrary(MyAllowSpecificOrigins);
-app.UseCommonLibrarySecuroman();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
