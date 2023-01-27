10c712438b05e99750fa1cbd95deb29044f514cf
Mohamed A. Laghrour <35415121+cppJoker@users.noreply.github.com>
2023-01-26 19:26:32 -0500

    Added role features

---
+++ src/LogService/Identity/Profiles/AutomapperProfile.cs
@@ -0,0 +1,19 @@
+﻿/*using AutoMapper;
+using CommonLibrary.AspNetCore.Identity;
+
+namespace LogService.Identity;
+
+public class GrpcUserProfile : Profile
+{
+    public GrpcUserProfile()
+    {
+        //Source -> Target
+        // CreateMap<UserService.GrpcUserBadge, UserBadge>()
+        //     .ForMember(dest=>  dest.UserId, opt=> opt.MapFrom(src=> src.UserId))
+        //     .ForMember(dest=>  dest.LogHandleId, opt=> opt.MapFrom(src=> src.LogHandleId))
+        //     .ForMember(dest=>  dest.SecretKey, opt=> opt.MapFrom(src=> src.SecretKey))
+        //     .ForMember(dest=>  dest.RoleIdentity, opt=> opt.MapFrom(src=> src.RoleIdentity))
+        //     .ForMember(dest=> dest.RoleIdentity.Roles, opt=> opt.MapFrom(src=> src.RoleIdentity.Roles))
+        //     .ForMember(dest=> dest.RoleIdentity.Permissions, opt=> opt.MapFrom(src=> src.RoleIdentity.Permissions));
+    }
+}*/
